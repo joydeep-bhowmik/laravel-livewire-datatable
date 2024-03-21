d523b381edd90b8d84d35206e52402bd81a0886d
Joydeep Bhowmik <45948068+joydeep-bhowmik@users.noreply.github.com>
2024-03-21 21:23:20 +0530

    Update datatable.blade.php

--- src/resources/views/livewire/datatable.blade.php
+++ src/resources/views/livewire/datatable.blade.php
@@ -116,14 +116,7 @@
                                             </option>
                                         @endforeach
                                     </select>
-                                    <div
-                                        class="pointer-events-none absolute bottom-[12px] right-0 flex items-center px-2 text-gray-700">
-                                        <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 9l-7 7-7-7"></path>
-                                        </svg>
-                                    </div>
+
                                 </div>
                             @elseif(in_array($filter->type, ['checkbox', 'radio']))
                                 <div class="flex gap-3">
